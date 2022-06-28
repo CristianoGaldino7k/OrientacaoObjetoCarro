83feb5fa21b41e1a1cc9f726ae90f0c57d9bef16
Cristiano Galdino <cristianocoringa7k@gmail.com>
2022-06-28 20:52:26 -0300

    projeto carro testato funcional

---
+++ src/Veiculo.java
@@ -0,0 +1,17 @@
+public class Veiculo {
+    public static void main(String[] args) {
+        Carros veiculos = new Carros();
+        veiculos.condicao();
+        veiculos.nome();
+        veiculos.marca();
+        veiculos.cor();
+        veiculos.tipoDeCombustivel();
+        veiculos.portas();
+        veiculos.direcao();
+        veiculos.arCondicionado();
+        veiculos.estado();
+        veiculos.ano();
+        veiculos.valor();
+        veiculos.detalhesEscolhidos();
+    }
+}
